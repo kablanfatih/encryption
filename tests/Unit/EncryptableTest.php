329adf36c993e977dcfc0c50e7b05d05ebf2d0ba
Fatih Kablan <kablanfatih34@gmail.com>
2020-05-04 23:36:04 +0300

    Unit Test writing

--- tests/Unit/EncryptableTest.php
+++ tests/Unit/EncryptableTest.php
@@ -1,30 +1,121 @@
 <?php
 
+namespace Kablanfatih\Encryption\tests\Unit;
 
-namespace Encryption\tests\Unit;
-
-
-use Encryption\src\Encryptable;
-use Tests\TestCase;
+use Kablanfatih\Encryption\Encryptable;
+use Illuminate\Support\Facades\Crypt;
+use Kablanfatih\Encryption\Providers\EncryptableServiceProvider;
+use Orchestra\Testbench\TestCase;
 
 class EncryptableTest extends TestCase
 {
-
     use Encryptable;
 
     protected function setUp(): void
     {
         parent::setUp();
+        $this->app->register(EncryptableServiceProvider::class);
     }
 
-    public function testifJson_ifJsonIsNotEmpty_shouldReturnTrue()
+    /**
+     * @test
+     */
+    public function testIsJson_ShouldReturnTrue_WhenJsonNotNull()
     {
-        $body = json_encode(['field' => 'json']);
-        $this->assertTrue($this->ifJson($body));
+        $json = json_encode([
+            'key' => 'value'
+        ]);
+        $isJson = $this->is_json($json);
+
+        $this->assertTrue($isJson);
     }
-    public function testifJson_ifJsonIsEmpty_shouldReturnFalse()
+
+    /**
+     * @test
+     */
+    public function testIsJson_ShouldReturnFalse_WhenJsonNull()
     {
-        $body = json_encode(null);
-        $this->assertFalse($this->ifJson($body));
+        $json = json_encode(null);
+        $isJson = $this->is_json($json);
+
+        $this->assertFalse($isJson);
+    }
+
+    /**
+     * @test
+     */
+    public function testDecryptField_ShouldReturnDecodedJson_WhenValueIsJson()
+    {
+        $json = json_encode([
+            'key' => 'value'
+        ]);
+        $encrypt = Crypt::encrypt($json);
+        $decryptField = $this->decryptField($encrypt);
+
+        $this->assertEquals(json_decode($json), $decryptField);
+    }
+
+    /**
+     * @test
+     */
+    public function testDecryptField_ShouldReturnDecryptedValue_WhenValueIsArray()
+    {
+        $array = [
+            'key' => 'value'
+        ];
+        $encrypt = Crypt::encrypt($array);
+        $decryptField = $this->decryptField($encrypt);
+
+        $this->assertEquals($array, $decryptField);
+    }
+
+    /**
+     * @test
+     */
+    public function testDecryptField_ShouldReturnDecryptedValue_WhenValueIsNotJson()
+    {
+        $value = "string";
+        $encrypt = Crypt::encrypt($value);
+        $decryptField = $this->decryptField($encrypt);
+
+        $this->assertEquals($value, $decryptField);
+    }
+
+    /**
+     * @test
+     */
+    public function testDecryptValue_ShouldReturnDecryptValue_IfConfigSettingsIsTrue()
+    {
+        $value = 1234;
+        $encrypt = Crypt::encrypt($value);
+        $decryptValue = $this->decryptValue($encrypt);
+
+        $this->assertEquals($value, $decryptValue);
+    }
+
+    /**
+     * @test
+     */
+    public function testDecryptValue_ShouldReturnEncryptValue_IfConfigSettingsIsFalse()
+    {
+
+        config(['encryption.encrypt' => false]);
+        $value = 1234;
+        $encrypt = Crypt::encrypt($value);
+        $decryptValue = $this->decryptValue($encrypt);
+
+        $this->assertEquals($encrypt, $decryptValue);
+    }
+
+    /**
+     * @test
+     */
+    public function testDecryptValue_ShouldReturnDecryptValue_IfValueIsNotNull()
+    {
+        $value = 1234;
+        $encrypt = Crypt::encrypt($value);
+        $decryptValue = $this->decryptValue($encrypt);
+
+        $this->assertEquals($value, $decryptValue);
     }
 }
